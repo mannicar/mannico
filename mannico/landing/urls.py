6df4d16fcd9c8184ee9a848cd1065ef06adef698
Carlo Mannino <mannicar@gmail.com>
2018-09-09 02:45:29 -0400

    Horsing around

--- mannico/landing/urls.py
+++ mannico/landing/urls.py
@@ -3,5 +3,5 @@ from landing import views
 
 
 urlpatterns = [
-
+    path('', views.index, name='index')
 ]
--- mannico/landing/views.py
+++ mannico/landing/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    context = {
+        'context_test_variable' : 'None, yet!'
+    }
+    return render(request, "index.html", context=context)
