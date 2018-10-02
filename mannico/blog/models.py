50865dab55374c874db22d7496dd06068781f9f9
Carlo Mannino <manninosca@gmail.com>
2018-10-02 17:17:35 -0400

    models

--- mannico/blog/models.py
+++ mannico/blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+
+class Article(models.Model):
+    title       = models.CharField(max_length=100, help_text="Article Title")
+    author      = models.CharField(max_length=50, help_text="Article Author")
+    pub_date    = models.DateTimeField(default=datetime.utcnow())
+    body        = models.TextField()
+    #id = models.IntegerField(primary_key=True)
+    views       = models.AutoField(default=0)
